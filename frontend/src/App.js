5c34a7399f2aabbb9ddb49fe70345322c5ee8b40
elfetetotaj <elfetetotaj@hotmail.com>
2022-09-21 17:59:27 +0200

    Impelent signIn fronted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import HomeView from './views/HomeView';
 import AnimalView from './views/AnimalView';
 import CartView from './views/CartView';
+import SignInView from './views/SignInView';
+import { signout } from './actions/userActions';
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -22,13 +30,29 @@ function App() {
               <span className="badge">{cartItems.length}</span>
             )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
           <Routes>
             <Route path="/cart/:id" element={<CartView />} />
             <Route path="/animal/:id" element={<AnimalView />} />
+            <Route path="/signin" element={<SignInView />} />
             <Route path="/" element={<HomeView />} exact />
           </Routes>
         </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,14 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { animalListReducer, animalDetailsReducer } from './reducers/animalReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
 const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -15,6 +21,7 @@ const reducer = combineReducers({
   animalList: animalListReducer,
   animalDetails: animalDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
