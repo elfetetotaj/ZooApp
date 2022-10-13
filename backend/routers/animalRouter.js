4e691d63b553c35170c3a144c4e2dd5913cc3549
elfetetotaj <elfetetotaj@hotmail.com>
2022-10-14 01:37:32 +0200

    Search and Filters

--- backend/routers/animalRouter.js
+++ backend/routers/animalRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import Animal from '../models/animalModel.js';
+import User from '../models/userModel.js';
 import { isAdmin, isAuth, isSellerOrAdmin } from '../utils.js';
 
 const animalRouter = express.Router();
@@ -9,14 +10,48 @@ const animalRouter = express.Router();
 animalRouter.get(
   '/',
   expressAsyncHandler(async (req, res) => {
+    const name = req.query.name || '';
+    const category = req.query.category || '';
     const seller = req.query.seller || '';
+    const adopt = req.query.adopt || '';
+    const min =
+      req.query.min && Number(req.query.min) !== 0 ? Number(req.query.min) : 0;
+    const max =
+      req.query.max && Number(req.query.max) !== 0 ? Number(req.query.max) : 0;
+    const rating =
+      req.query.rating && Number(req.query.rating) !== 0
+        ? Number(req.query.rating)
+        : 0;
+    const nameFilter = name ? { name: { $regex: name, $options: 'i' } } : {};
     const sellerFilter = seller ? { seller } : {};
+    const categoryFilter = category ? { category } : {};
+    const priceFilter = min && max ? { price: { $gte: min, $lte: max } } : {};
+    const ratingFilter = rating ? { rating: { $gte: rating } } : {};
+    const sortAdopt =
+      adopt === 'lowest'
+        ? { price: 1 }
+        : adopt === 'highest'
+        ? { price: -1 }
+        : adopt === 'toprated'
+        ? { rating: -1 }
+        : { _id: -1 };
     const animals = await Animal.find({ 
-      ...sellerFilter }).populate(
-        'seller',
-        'seller.name seller.logo'
-      );
+      ...sellerFilter,
+      ...nameFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    }).populate('seller', 'seller.name seller.logo'
+      ).sort(sortAdopt);
     res.send(animals);
+  })
+);
+
+animalRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Animal.find().distinct('category');
+    res.send(categories);
   })
 );
 
@@ -24,8 +59,19 @@ animalRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
     // await Animal.remove({});
-    const createdAnimals = await Animal.insertMany(data.animals);
-    res.send({ createdAnimals });
+    const seller = await User.findOne({ isSeller: true });
+    if (seller) {
+      const animals = data.animals.map((animal) => ({
+        ...animal,
+        seller: seller._id,
+      }));
+      const createdAnimals = await Animal.insertMany(animals);
+      res.send({ createdAnimals });
+    } else {
+      res
+        .status(500)
+        .send({ message: 'No seller found. first run /api/users/seed' });
+    }
   })
 );
 
